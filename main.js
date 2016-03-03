2b5b50510870e5e5e637263ce6a1a8c1b361fb32
joseph <joseph>
2016-03-03 15:58:41 +1300

    started .js, added button to .html

---
+++ main.js
@@ -0,0 +1,8 @@
+function start () {
+	document.getElementById("display").innerHTML='0';//setup HTML
+	//document.addEventListener("click", buttonPress);//setup buttonpress function
+}
+
+function buttonPress (input) {
+	console.log(input);
+}
